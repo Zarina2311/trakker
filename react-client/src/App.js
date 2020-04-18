5d6c6d8f60dae71a9d44723eb8201094c603d3c2
Zarina Gumbatova <26606767+Zarina2311@users.noreply.github.com>
2020-04-18 01:46:53 -0700

    added buttons, headers, and styling

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,8 +1,28 @@
 import React from "react";
 import "./App.css";
+import { Button } from "reactstrap";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div>
+        <Button className="button2" color="info">
+          Log In
+        </Button>
+      </div>
+      <h1>Trakker</h1>
+      <br />
+      <br />
+      <h3>Job Search Tracking Just Got Easy</h3>
+      <br />
+      <h5>
+        Trakker is an efficient tool to organize and keep track of your next job
+        opportunity
+      </h5>
+      <br />
+      <Button className="button1">Sign Up - It's Free</Button>
+    </div>
+  );
 }
 
 export default App;
