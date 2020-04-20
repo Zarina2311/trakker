3577dee114dd02dbd603637e064678a92114fd65
Zarina Gumbatova <26606767+Zarina2311@users.noreply.github.com>
2020-04-19 18:45:32 -0700

    fixed background color, added card to login

--- react-client/src/components/Login/index.js
+++ react-client/src/components/Login/index.js
@@ -1,8 +1,43 @@
 import React from "react";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Card,
+  CardBody,
+  Label,
+  Input,
+} from "reactstrap";
 import "./style.css";
 
 function Login() {
-  return <div className="Login">login</div>;
+  return (
+    <div className="Login">
+      <br />
+      <br />
+      <br />
+      <Form className="Form">
+        <br />
+        <Card>
+          <CardBody>
+            <h2 className="welcome">Welcome back</h2>
+            <br />
+            <br />
+            <FormGroup>
+              <Label>Email</Label>
+              <Input type="email" placeholder="Email" />
+            </FormGroup>
+            <FormGroup>
+              <Label>Password</Label>
+              <Input type="password" placeholder="Password" />
+            </FormGroup>
+            <Button className="btn-lg btn-info btn-block">Log In</Button>
+            <br />
+          </CardBody>
+        </Card>
+      </Form>
+    </div>
+  );
 }
 
 export default Login;
--- react-client/src/components/SignUp/index.js
+++ react-client/src/components/SignUp/index.js
@@ -6,24 +6,29 @@ import "./style.css";
 
 function SignUp() {
   return (
-    <Form className="SignUp">
-      <h1>Trakker</h1>
-      <h2>Welcome</h2>
-      <FormGroup>
-        <Label>Email</Label>
-        <Input type="email" placeholder="Email" />
-      </FormGroup>
-      <FormGroup>
-        <Label>Password</Label>
-        <Input type="password" placeholder="Password" />
-      </FormGroup>
-      <Button className="btn-lg btn-info btn-block">Sign Up</Button>
+    <div className="SignUp">
       <br />
-      <div className="text-center pt-3">Or Login with your social account</div>
-      <br />
-      <LinkedInLoginButton className="mt-3 mb-3" />
-      <GoogleLoginButton className="mt-3 mb-3" />
-    </Form>
+      <Form className="Form">
+        <h1 className="header">Trakker</h1>
+        <h2>Welcome</h2>
+        <FormGroup>
+          <Label>Email</Label>
+          <Input type="email" placeholder="Email" />
+        </FormGroup>
+        <FormGroup>
+          <Label>Password</Label>
+          <Input type="password" placeholder="Password" />
+        </FormGroup>
+        <Button className="btn-lg btn-info btn-block">Sign Up</Button>
+        <br />
+        <div className="text-center pt-3">
+          Or Login with your social account
+        </div>
+        <br />
+        <LinkedInLoginButton className="mt-3 mb-3" />
+        <GoogleLoginButton className="mt-3 mb-3" />
+      </Form>
+    </div>
   );
 }
 
