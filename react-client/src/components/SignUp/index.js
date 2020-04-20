be67df31e5f09c1e986c4b4a57496c1010ca2199
Zarina Gumbatova <26606767+Zarina2311@users.noreply.github.com>
2020-04-19 17:43:14 -0700

    added buttons to login

--- react-client/src/components/SignUp/index.js
+++ react-client/src/components/SignUp/index.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
 import "./style.css";
 
 function SignUp() {
   return (
-    <div className="SignUp">
+    <Form className="SignUp">
       <h1>Trakker</h1>
       <h2>Welcome</h2>
-    </div>
+      <FormGroup>
+        <Label>Email</Label>
+        <Input type="email" placeholder="Email" />
+      </FormGroup>
+      <FormGroup>
+        <Label>Password</Label>
+        <Input type="password" placeholder="Password" />
+      </FormGroup>
+      <Button className="btn-lg btn-info btn-block">Sign Up</Button>
+      <div className="text-center pt-3">Or Login with your social account</div>
+    </Form>
   );
 }
 
