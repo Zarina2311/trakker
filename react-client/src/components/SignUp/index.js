56e4f63b6b79b3297b797719bd2a7d91eea34193
Zarina Gumbatova <26606767+Zarina2311@users.noreply.github.com>
2020-04-19 17:56:26 -0700

    added social login buttons

--- react-client/src/components/SignUp/index.js
+++ react-client/src/components/SignUp/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import { GoogleLoginButton } from "react-social-login-buttons";
+import { LinkedInLoginButton } from "react-social-login-buttons";
 import "./style.css";
 
 function SignUp() {
@@ -16,7 +18,11 @@ function SignUp() {
         <Input type="password" placeholder="Password" />
       </FormGroup>
       <Button className="btn-lg btn-info btn-block">Sign Up</Button>
+      <br />
       <div className="text-center pt-3">Or Login with your social account</div>
+      <br />
+      <LinkedInLoginButton className="mt-3 mb-3" />
+      <GoogleLoginButton className="mt-3 mb-3" />
     </Form>
   );
 }
