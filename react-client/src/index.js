f10bbaa402cc99dc25d1fdd0cd500909cc5c1f40
Zarina Gumbatova <26606767+Zarina2311@users.noreply.github.com>
2020-05-06 00:33:01 -0700

    removed links to sign up and login forms

--- react-client/src/index.js
+++ react-client/src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Home from "./components/Home";
-import Login from "./components/Login";
-import SignUp from "./components/SignUp";
 import Board from "./components/Board";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
@@ -13,8 +11,6 @@ ReactDOM.render(
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/signup" component={SignUp} />
         <Route exact path="/board" component={Board} />
       </Switch>
     </Router>
