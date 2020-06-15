4e99d2bd749027345e6f12a0f1f31613a1af04d5
Zarina Gumbatova <26606767+Zarina2311@users.noreply.github.com>
2020-06-14 21:18:51 -0700

    update routes

--- react-client/src/api-fetcher.js
+++ react-client/src/api-fetcher.js
@@ -3,31 +3,30 @@ const checkForErrors = (res) => {
   return res.json();
 };
 
-const API_ROOT = "https://job-trakker-api.herokuapp.com"; // heroku
-// const API_ROOT = "http://localhost:3001";
+//const API_ROOT = "https://job-trakker-api.herokuapp.com"; // heroku
+const API_ROOT = "http://localhost:3001";
 
 //GET
 export function getColumns({ auth0_id }) {
-  return fetch(`${API_ROOT}/user/${auth0_id}`)
+  return fetch(`${API_ROOT}/user/${auth0_id}/column`)
     .then(checkForErrors)
     .catch((error) => console.log("error", error));
 }
 
 export function getCardsForColumn({ auth0_id, columnId }) {
-  return fetch(`${API_ROOT}/user/${auth0_id}/${columnId}`)
+  return fetch(`${API_ROOT}/user/${auth0_id}/column/${columnId}`)
     .then(checkForErrors)
     .catch((error) => console.log("error", error));
 }
 
 //POST column
 export function addColumn({ auth0_id, name }) {
-  return fetch(`${API_ROOT}/user/${auth0_id}`, {
+  return fetch(`${API_ROOT}/user/${auth0_id}/column`, {
     method: "POST",
     headers: new Headers({
       "Content-Type": "application/x-www-form-urlencoded",
     }),
     body: new URLSearchParams({ name }),
-    redirect: "follow",
   })
     .then(checkForErrors)
     .catch((error) => console.log("error", error));
@@ -35,21 +34,20 @@ export function addColumn({ auth0_id, name }) {
 
 //POST card
 export function addCard({ auth0_id, name, columnId }) {
-  return fetch(`${API_ROOT}/user/${auth0_id}/${columnId}`, {
+  return fetch(`${API_ROOT}/user/${auth0_id}/column/${columnId}/card`, {
     method: "POST",
     headers: new Headers({
       "Content-Type": "application/x-www-form-urlencoded",
     }),
     body: new URLSearchParams({ name }),
-    redirect: "follow",
   })
     .then(checkForErrors)
     .catch((error) => console.log("error", error));
 }
 
 // PUT card (drag and drop)
-export function moveCard({ auth0_id, columnId, cardId, newColumnId }) {
-  return fetch(`${API_ROOT}/user/${auth0_id}/${columnId}/${cardId}`, {
+export function moveCard({ auth0_id, cardId, newColumnId }) {
+  return fetch(`${API_ROOT}/user/${auth0_id}/card/${cardId}`, {
     method: "PUT",
     headers: new Headers({
       "Content-Type": "application/x-www-form-urlencoded",
@@ -62,13 +60,15 @@ export function moveCard({ auth0_id, columnId, cardId, newColumnId }) {
 
 //DELETE column
 export function deleteColumn({ auth0_id, columnId }) {
-  return fetch(`${API_ROOT}/user/${auth0_id}/${columnId}`, { method: "DELETE" })
+  return fetch(`${API_ROOT}/user/${auth0_id}/column/${columnId}`, {
+    method: "DELETE",
+  })
     .then(checkForErrors)
     .catch((error) => console.log("error", error));
 }
 
-export function deleteCard({ auth0_id, columnId, cardId }) {
-  return fetch(`${API_ROOT}/user/${auth0_id}/${columnId}/${cardId}`, {
+export function deleteCard({ auth0_id, cardId }) {
+  return fetch(`${API_ROOT}/user/${auth0_id}/card/${cardId}`, {
     method: "DELETE",
   })
     .then(checkForErrors)
